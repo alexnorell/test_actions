74700d2601bc40a6644eb0547897ca569de06b5b
Alex Norell <alex@norell.co>
2024-09-25 17:35:25 -0700

    Add bucket

--- infra/main.tf
+++ infra/main.tf
@@ -16,3 +16,13 @@ provider "aws" {
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My S3 bucket"
+    Environment = "Dev"
+  }
+}
